6bf68c9e4395051f10c0e981a9ba691500e6f174
atm <144315333+nmtuonggh@users.noreply.github.com>
2024-07-30 01:31:52 +0700

    no message

---
+++ SpiderMy/Assets/_Game/_Scripts/Enemy/State/PatrollState.cs
@@ -0,0 +1,17 @@
+﻿
+using NodeCanvas.BehaviourTrees;
+using NodeCanvas.Framework;
+using UnityEngine;
+
+namespace SFRemastered._Game._Scripts.Enemy.State
+{
+    public class PatrollState : ActionNode 
+    {
+        protected override Status OnExecute(Component agent, IBlackboard blackboard)
+        {
+            Debug.Log("OnExecute PatrollState");
+            action.Execute(agent, blackboard);
+            return Status.Success;
+        }
+    }
+}
